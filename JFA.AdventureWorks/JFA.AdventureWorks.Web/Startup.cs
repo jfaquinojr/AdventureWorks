9d847ffde2414ecbc5b09e0b3c0c2de05bd2e7e0
Jojo Aquino <jfaquinojr@gmail.com>
2016-01-19 22:43:07 +0800

    add EF generated

---
+++ JFA.AdventureWorks/JFA.AdventureWorks.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JFA.AdventureWorks.Web.Startup))]
+namespace JFA.AdventureWorks.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
